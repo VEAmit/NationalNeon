0fdfeb2a0e89f5e1e945bc9adf67eaca6a980e19
Amit <amitkumar7@virtualemployee.com>
2019-01-15 13:49:40 +0530

    drag and drop image and feddback sheet completed

---
+++ NationalNeonRepository/NationalNeon.Repository/Migrations/20190108131103_jobfileuploadchangesmigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NationalNeon.Repository.Migrations
+{
+    public partial class jobfileuploadchangesmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_JobFileUploads_JobId",
+                table: "JobFileUploads");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobFileUploads_JobId",
+                table: "JobFileUploads",
+                column: "JobId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_JobFileUploads_JobId",
+                table: "JobFileUploads");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobFileUploads_JobId",
+                table: "JobFileUploads",
+                column: "JobId",
+                unique: true,
+                filter: "[JobId] IS NOT NULL");
+        }
+    }
+}
--- NationalNeonRepository/NationalNeon.Web/Controllers/JobsController.cs
+++ NationalNeonRepository/NationalNeon.Web/Controllers/JobsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,14 +11,16 @@ using Microsoft.AspNetCore.Http;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace NationalNeon.Web.Controllers
 {
     public class JobsController : Controller
     {
         private readonly IJobBusiness ijobBusiness;
-        private readonly IHostingEnvironment ihostingEnv;
         private readonly ITaskBusiness itaskBusiness;
+        private readonly IHostingEnvironment ihostingEnv;
         public JobsController(IJobBusiness ijobBusiness, IHostingEnvironment ihostingEnv, ITaskBusiness itaskBusiness)
         {
             this.ijobBusiness = ijobBusiness;
@@ -60,23 +62,6 @@ namespace NationalNeon.Web.Controllers
                 data = model
             });
         }
-
-        public ActionResult Detail(int jobId)
-        {
-
-            var model = ijobBusiness.GetJob(jobId);
-            if (model.Tasks.Count > 0)
-            {
-                var completedTaskTotalHours = model.Tasks.Where(row => row.Completed == 1).Sum(row => Convert.ToDecimal(row.BudgetedHours));
-
-                var totalHours = model.Tasks.Sum(row => Convert.ToDecimal(row.BudgetedHours));
-                ViewBag.progress = Convert.ToString(Convert.ToInt32(Math.Round(completedTaskTotalHours / totalHours, 2) * 100)) + "%";
-
-                model.Tasks = itaskBusiness.GetAll().Where(row => row.jobId == jobId).ToList();
-            }
-            return View(model);
-        }
-
 
         [HttpPost]
         public ActionResult AddJobs(JobViewModel jobModel)
@@ -177,7 +162,7 @@ namespace NationalNeon.Web.Controllers
                 {
                     string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
                     filename = this.EnsureCorrectFilename(filename);
-                    fileUniquePath = "\\uploads\\" + jobId + "_" + Path.GetFileNameWithoutExtension(filename.Replace("_", "")) + "_" + DateTime.UtcNow.ToFileTime().ToString() + Path.GetExtension(filename);
+                    fileUniquePath = "\\uploads\\" + jobId + "_" + Path.GetFileNameWithoutExtension(filename.Replace("_","")) + "_" + DateTime.UtcNow.ToFileTime().ToString() + Path.GetExtension(filename);
                     filePath = Path.Combine(this.ihostingEnv.WebRootPath + fileUniquePath);
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
@@ -216,13 +201,11 @@ namespace NationalNeon.Web.Controllers
                 filename = filename.Substring(filename.LastIndexOf("\\") + 1);
             return filename;
         }
-
-
-      
         private bool UpdateJobFilePath(int jobId, string filePath)
         {
             try
             {
+                
                 JobFileUploadModel jobFileUploadModel = new JobFileUploadModel();
                 jobFileUploadModel.JobId = jobId;
                 jobFileUploadModel.FilePath = filePath;
@@ -236,8 +219,24 @@ namespace NationalNeon.Web.Controllers
             }
 
         }
+        public ActionResult Detail(int jobId)
+        {
+            
+            var model = ijobBusiness.GetJob(jobId);
+           
+            if (model.Tasks.Count>0)
+            {
+                var completedTaskTotalHours = model.Tasks.Where(row => row.Completed == 1).Sum(row => Convert.ToDecimal(row.BudgetedHours));
 
+                var totalHours = model.Tasks.Sum(row => Convert.ToDecimal(row.BudgetedHours));
+                ViewBag.progress = Convert.ToString(Convert.ToInt32(Math.Round(completedTaskTotalHours / totalHours, 2) * 100)) + "%";
 
+                model.Tasks = itaskBusiness.GetAll().Where(row => row.jobId == jobId).ToList();
+            }
+
+           // ViewBag.jobFiles = JsonConvert.SerializeObject(model.JobFileUpload);
+            return View(model);
+        }
         [HttpPost]
         public ActionResult DeleteUploadedFile(int jobFileUploadId)
         {
@@ -249,7 +248,7 @@ namespace NationalNeon.Web.Controllers
                     success = true
                 });
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return Json(new
                 {
@@ -258,5 +257,80 @@ namespace NationalNeon.Web.Controllers
             }
         }
 
+        [HttpGet]
+        public ActionResult GetJobFiles(int jobId)
+        {
+            try
+            {
+                var model = ijobBusiness.GetJob(jobId);
+                if (model.JobFileUpload.Count > 0)
+                {
+                    return Json(new { data = model.JobFileUpload, dataCount = model.JobFileUpload.Count }, new JsonSerializerSettings() { Formatting = Formatting.Indented });
+                }
+                else
+                {
+                    return Json(new { data = "", dataCount = 0 });
+                }
+            }
+            catch(Exception ex)
+            {
+                return Json(new { data = "Error", dataCount = -1 });
+            }
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> UploadFiles(string jobId)
+        {
+            string filePath = string.Empty, fileDatabasePath = string.Empty, fileUniquePath = string.Empty;
+            var files = Request.Form.Files;
+            foreach (var file2 in files)
+            {
+                string filename = ContentDispositionHeaderValue.Parse(file2.ContentDisposition).FileName.Trim('"');
+                filename = this.EnsureCorrectFilename(filename);
+                fileUniquePath = "\\uploads\\"  + Path.GetFileNameWithoutExtension(filename.Replace("_", "")) + "_" + DateTime.UtcNow.ToFileTime().ToString() + Path.GetExtension(filename);
+                filePath = Path.Combine(this.ihostingEnv.WebRootPath + fileUniquePath);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file2.CopyToAsync(stream);
+                }
+            }
+     
+            if (!string.IsNullOrEmpty(jobId))
+            {
+                if (UpdateJobFilePath(Convert.ToInt32(jobId), fileUniquePath))
+                {
+                    return Json(new
+                    {
+                        success = true
+                    });
+                }
+                
+            }
+            return Json(new
+            {
+                error = true
+            });
+        }
+        public IActionResult CompleteTask(int taskId)
+        {
+            try
+            {
+                itaskBusiness.updateincompleteTask(taskId);
+                return Json(new
+                {
+                    success = true
+                });
+            }
+            catch(Exception ex)
+            {
+                return Json(new
+                {
+                    success = false,
+                });
+
+            }
+            
+        }
+        
     }
 }
